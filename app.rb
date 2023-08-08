e696baa312021f9a213f60a254102b01befb2209
Juan Alvarado Caraballo <104938751+jalvarado9000@users.noreply.github.com>
2023-08-08 02:09:11 +0000

    almost done

--- app.rb
+++ app.rb
@@ -1,9 +1,60 @@
 require "sinatra"
 require "sinatra/reloader"
 
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  "<h2>howdy</h2>"
+end
+
+get("/rock") do
+  @yourMove = "rock"
+  moves = ["rock", "paper", "scissors"]
+  @pcMove = moves.sample
+
+  if @pcMove == "rock"
+    @result = "tied!"
+  elsif @pcMove == "paper"
+    @result = "lost!"
+  else
+    @result = "won!"
+  end
+
+  erb(:zebra)
+
+end
+
+get("/paper") do
+  @yourMove = "paper"
+  moves = ["rock", "paper", "scissors"]
+  @pcMove = moves.sample
+
+
+
+  if @pcMove == "paper"
+    @result = "tied!"
+  elsif @pcMove == "scissors"
+    @result = "lost!"
+  else
+    @result = "won!"
+  end
+
+  erb(:zebra)
+end
+
+get("/scissors") do
+  @yourMove = "scissors"
+  moves = ["rock", "paper", "scissors"]
+  @pcMove = moves.sample
+
+
+
+  if @pcMove == "scissors"
+    @result = "tied!"
+  elsif @pcMove == "rock"
+    @result = "lost!"
+  else
+    @result = "won!"
+  end
+
+  erb(:zebra)
 end
